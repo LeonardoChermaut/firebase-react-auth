f511cfd530c7ecb08239102b0700d567204392cb
Leonardo Chermaut <chermauts@hotmail.com>
2023-03-05 21:36:40 -0300

    adjust: media query styled buttons and table

--- src/pages/employee-list/Employee.List.styled.js
+++ src/pages/employee-list/Employee.List.styled.js
@@ -1,15 +1,14 @@
 import styled from "styled-components";
-import { Container, Table, Button } from "react-bootstrap"
+import { Container, Table, Button } from "react-bootstrap";
 
 export const TitleTableEmployee = styled.h2`
   text-align: center;
   margin: 2rem;
-
 `;
 export const ContainerTableEmployee = styled(Container)`
-    margin-top: 3rem;
-    align-items: center;
-    justify-content: center;
+  margin-top: 3rem;
+  align-items: center;
+  justify-content: center;
 `;
 
 export const TableEmployee = styled(Table)`
@@ -19,11 +18,16 @@ export const TableEmployee = styled(Table)`
   align-items: center;
   justify-content: center;
   border-radius: 1rem;
-  
+  @media (max-width: 756px) {
+    width: auto;
+  }
 `;
 
 export const ButtonAction = styled(Button)`
-    width: 90%;
-    margin: .2rem auto;
-    padding: .2rem;
+  width: 90%;
+  margin: 0.5rem auto;
+  padding: 0.2rem;
+  @media (max-width: 756px) {
+    width: 100%;
+  }
 `;
